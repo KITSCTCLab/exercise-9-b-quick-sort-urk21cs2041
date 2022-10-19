5248eb01856a592003548aedba780a7a60514ad2
urk21cs2041 <110519141+urk21cs2041@users.noreply.github.com>
2022-10-19 07:25:30 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,21 @@
 from typing import List
+def parition(data, low, high) -> List[int]:
+    i = (low-1)
+    pivot = data[high]
+    for j in range(low, high):
+        if data[j] < pivot:
+            i = i+1
+            data[i], data[j] = data[j], data[i]
+    data[i+1], data[high] = data[high], data[i+1]
+    return (i+1)
 
 def quick_sort(data, low, high) -> List[int]:
     # Write code here
+    if low < high:
+        pi = parition(data, low, high)
+        quick_sort(data, low, pi-1)
+        quick_sort(data, pi+1, high)
+    return data
 
 
 input_data = input()
